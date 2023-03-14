abd4dfa81757430ec3db0040771afbc24845a2d8
Kumari Aanchal <84965729+KumariAanchal@users.noreply.github.com>
2023-03-14 20:19:48 +0530

    Add files via upload

---
+++ lambdaexample.java
@@ -0,0 +1,20 @@
+public class lambdaexample {
+    public static void main (String args[]){
+        Outer outer=new Outer();
+        outer.test();
+    }
+    
+}
+class Outer{
+    int outer_x=100;
+    void test(){
+        Inner inner = new Inner();
+        inner.display();
+    }
+    class Inner{
+        void display(){
+            System.out.println("display:outer_x="+outer_x);
+        }
+
+    }
+}
