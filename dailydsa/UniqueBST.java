29dc24cf68c0d80b1f8dcb94d00aa8ed4f6d54f7
Kumari Aanchal <84965729+KumariAanchal@users.noreply.github.com>
2023-08-05 15:12:37 +0530

    Create UniqueBST.java

---
+++ dailydsa/UniqueBST.java
@@ -0,0 +1,30 @@
+class Solution {
+     Map<Pair<Integer, Integer>, List<TreeNode>> dp; 
+    public List<TreeNode> generateTrees(int n) {
+        dp = new HashMap<>();
+        return helper(1, n);
+    }
+    
+    public List<TreeNode> helper(int start, int end) {
+        List<TreeNode> variations = new ArrayList<>();
+        if (start > end) {
+            variations.add(null);
+            return variations;
+        }
+        if (dp.containsKey(new Pair<>(start, end))) {
+            return dp.get(new Pair<>(start, end));
+        }
+        for (int i = start; i <= end; ++i) {
+            List<TreeNode> leftSubTrees = helper(start, i - 1);
+            List<TreeNode> rightSubTrees = helper(i + 1, end);
+            for (TreeNode left: leftSubTrees) {
+                for (TreeNode right: rightSubTrees) {
+                    TreeNode root = new TreeNode(i, left, right);
+                    variations.add(root);
+                }
+            }
+        }
+        dp.put(new Pair<>(start, end), variations);
+        return variations;
+    }
+}
